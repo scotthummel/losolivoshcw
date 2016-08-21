31888a158cb4e9eb384f19e51f5a5dffce9274ff
Scott Hummel <scotthummel@icloud.com>
2016-08-21 09:51:16 -0700

    Added mailer

---
+++ resources/views/contact/mailer.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>Message From the Website</h1>
+
+    <p>Name: {{ $name }}</p>
+
+    <p>Email: {{ $email }}</p>
+
+    <p>Phone: {{ $phone }}</p>
+
+    <p>Message: {{ $comments }}</p>
+</body>
+</html>
