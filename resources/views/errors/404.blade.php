cc375919ad74377bae9fd52a83b45f39e0df2d82
Scott Hummel <scotthummel@icloud.com>
2016-08-21 14:29:16 -0700

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,29 @@
+@extends('templates.master')
+
+@section('title', 'Los Olivos Hand Car Wash :: Page Not Found :: Phoenix, AZ')
+
+@section('description', 'Page Not Found.')
+
+@section('content')
+    <div class="row body">
+        <div class="col xs-6 hidden-xs col-sm-3 col-md-3">
+            <img src="/images/coletta.jpg" class="img-responsive" alt="Coletta" />
+        </div>
+        <div class="col-xs-12 col-sm-9 col-md-9">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1 class="panel-title">
+                        Page Not Found
+                    </h1>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <p>We're sorry, but the page you are looking for could not found.  Please check the link or try back later if you believe you have reached this message in error.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
